ce0df4444a60b60600bca45ca0c85ca265377ea7
JArthurJohnston <joe.a.johnston@gmail.com>
2018-02-15 07:54:30 -0500

    Copied all the apollo code from the Grimoire project into this new project. Added gradle build scripts.

---
+++ src/test/java/com/paratussoftware/apollo/musical/serialization/SongWriterTest.java
@@ -0,0 +1,24 @@
+package com.paratussoftware.apollo.musical.serialization;
+
+import com.paratussoftware.apollo.musical.Melody;
+import org.junit.Test;
+
+import static com.paratussoftware.apollo.musical.Melody.Tab.*;
+import static org.junit.Assert.*;
+
+public class SongWriterTest {
+
+    @Test
+    public void testReadAndWriteMelody() {
+        final SongWriter songWriter = new SongWriter();
+        final String expectedMelodyName = "Go Go Power Rangers";
+        final Melody powerRangersJingle = new Melody(expectedMelodyName, E, E, D, E, G, E);
+
+        final String songFileName = "./resources/ranger.melody";
+        songWriter.write(powerRangersJingle, songFileName);
+
+        final Melody songFromFile = songWriter.read(songFileName);
+
+        assertEquals(expectedMelodyName, songFromFile.getName());
+    }
+}
